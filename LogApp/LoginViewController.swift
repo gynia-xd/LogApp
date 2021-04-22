ddafc2683f534b40b72e6bd57a143f5589983edd
Gynia_xd <Gynia_xd@mail.ru>
2021-04-22 16:30:09 +0300

    interface

--- LogApp/LoginViewController.swift
+++ LogApp/LoginViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
